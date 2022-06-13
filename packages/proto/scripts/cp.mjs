b833a1a80e538e7f3e37e03a51d740bfd0de7220
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-06-13 12:10:54 +0800

    Fix publish

--- packages/proto/scripts/cp.mjs
+++ packages/proto/scripts/cp.mjs
@@ -2,6 +2,6 @@ import fs from 'node:fs/promises'
 
 const items = await fs.readdir('dist')
 
-const tasks = items.map(item => fs.unlink(item).catch(() => {}))
+const tasks = items.map(item => fs.cp(`dist/${item}`, item, { force: true, recursive: true }))
 
 await Promise.all(tasks)
