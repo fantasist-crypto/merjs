546679c3883b918420b3a480c5bd30a85c3462ce
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-06-13 11:52:05 +0800

    Fix publish

--- packages/proto/scripts/prepublish.mjs
+++ packages/proto/scripts/prepublish.mjs
@@ -2,6 +2,6 @@ import fs from 'node:fs/promises'
 
 const items = await fs.readdir('dist')
 
-const tasks = items.map(item => fs.symlink(`dist/${item}`, item))
+const tasks = items.map(item => fs.symlink(`dist/${item}`, item).catch(() => {}))
 
 await Promise.all(tasks)
