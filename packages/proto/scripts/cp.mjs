e2e0a7fed5fd642207dfe131f2f8da9865af5aa1
Fantastic <100333503+fantasticist@users.noreply.github.com>
2022-07-01 16:33:12 +0800

    Update tsc build dir for DX

--- packages/proto/scripts/cp.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs/promises'
-
-const items = await fs.readdir('dist')
-
-const tasks = items.map(item => fs.cp(`dist/${item}`, item, { force: true, recursive: true }))
-
-await Promise.all(tasks)
